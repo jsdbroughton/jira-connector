46d0792950f1814ad792dbb97af52ce8ba5a3782
Jonathon Broughton <jonathon@stardotbmp.com>
2020-02-28 15:49:30 +0000

    fix: reference

--- types/index.d.ts
+++ types/index.d.ts
@@ -14,6 +14,7 @@ import {
   Issue,
   IssueType,
   Labels,
+  Priority,
   Project,
   Search,
   Sprint,
@@ -59,7 +60,7 @@ export default class JiraClient {
   password: any;
   permissions: any;
   permissionScheme: any;
-  priority: any;
+  priority: Priority;
   project: Project;
   projectCategory: any;
   projectValidate: any;
