e07d30a70ecf7a0d1a43217bc534b6528b6be655
Jonathon Broughton <jonathon@stardotbmp.com>
2020-02-28 14:58:49 +0000

    feature: types related to project roles

--- types/api/project.d.ts
+++ types/api/project.d.ts
@@ -1,5 +1,55 @@
 import { Callback } from "../callback";
 import { PageBeanVersion } from "../models";
+import { AvatarUrls } from "./user";
+
+export interface UpdatedProjectCategory {
+  self: string;
+  id: string;
+  description: string;
+  name: string;
+}
+export interface ProjectForScope {
+  self: string;
+  id: string;
+  key: string;
+  name: string;
+  projectTypeKey: string;
+  simplified: boolean;
+  avatarUrls: AvatarUrls;
+  projectCategory: UpdatedProjectCategory
+}
+export interface Scope {
+  type: string;
+  project: ProjectForScope;
+}
+
+export interface ProjectRoleGroup {
+  displayName: string;
+  name: string;
+}
+export interface RoleActor {
+  id: number;
+  displayName: string;
+  type: string;
+  name: string;
+  avatarUrl: string;
+  actorUser?: ProjectRoleUser;
+  actorGroup?: ProjectRoleGroup;
+}
+export interface ProjectRole {
+  self: string;
+  name: string;
+  id: number;
+  description: string;
+  actors: RoleActor[];
+  scope: Scope;
+  admin: boolean;
+  default: boolean;
+}
+
+export interface ProjectRoleMap {
+  [name: string]: [uri: string]
+}
 
 export class Project {
   getAllProjects(
@@ -61,9 +111,12 @@ export class Project {
     expand?: any[];
   }, callback?: Callback): Promise<PageBeanVersion>;
 
-  getRoles(opts: any, callback?: Callback): Promise<any>;
+  getRoles(opts: { projectIdOrKey: string | number}, callback?: Callback): Promise<ProjectRoleMap>;
 
-  getRole(opts: any, callback?: Callback): Promise<any>;
+  getRole(opts: { projectIdOrKey: string | number, id: number}, callback?: Callback): Promise<ProjectRole>;
+
+  getRoleDetails(opts: { projectIdOrKey: string | number, currentMember?: boolean, excludeConnectAddons?: boolean}, callback?: Callback): Promise<ProjectRole[]>;
+
 
   updateRole(opts: any, callback?: Callback): Promise<any>;
 
