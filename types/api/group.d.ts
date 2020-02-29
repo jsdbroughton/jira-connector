d40f997c2931df1015a91ddc742292b136752212
Jonathon Broughton <jonathon@stardotbmp.com>
2020-02-29 09:41:16 +0000

    ss

--- types/api/group.d.ts
+++ types/api/group.d.ts
@@ -2,36 +2,4 @@ import { Callback } from "../callback";
 import { AvatarUrls } from "./user";
 
 export class Group {
-  getMembers(
-    opts: {
-      groupName: string;
-      includeInactiveUsers?: boolean;
-      startAt?: number;
-      maxResults?: number;
-    },
-    callback?: Callback
-  ): Promise<UserDetailsPage>;
-}
-
-export interface UserDetailsPage {
-  self: string;
-  nextPage: string;
-  maxResults: number;
-  startAt: number;
-  total: number;
-  isLast: boolean;
-  values: UserDetails[];
-}
-
-export interface UserDetails {
-  self: string;
-  name?: string;
-  key?: string;
-  accountId: string;
-  emailAddress: string;
-  avatarUrls: AvatarUrls;
-  displayName: string;
-  active: boolean;
-  timeZone: string;
-  accountType: string;
 }
